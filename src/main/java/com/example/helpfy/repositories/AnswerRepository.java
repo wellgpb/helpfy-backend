c51ce4a06bf230d06336a5630a6f7a8af8c77fb5
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2023-01-29 11:49:18 -0300

    add pagination to repository methods

--- src/main/java/com/example/helpfy/repositories/AnswerRepository.java
+++ src/main/java/com/example/helpfy/repositories/AnswerRepository.java
@@ -2,10 +2,11 @@ package com.example.helpfy.repositories;
 
 import com.example.helpfy.models.Answer;
 import com.example.helpfy.models.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> findAnswersByAuthor(User author);
+    List<Answer> findAnswersByAuthor(User author, Pageable pageable);
 }
